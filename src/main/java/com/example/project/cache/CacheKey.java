d7d5aae0cece05711abd1ee9c107749a135416f0
Raukouski <raukouskiandrey@gmail.com>
2026-03-19 23:13:05 +0300

    Lab3.1

--- src/main/java/com/example/project/cache/CacheKey.java
+++ src/main/java/com/example/project/cache/CacheKey.java
@@ -4,7 +4,7 @@ import java.util.Arrays; // Добавьте импорт
 import java.util.List;
 
 public record CacheKey(Class<?> entityClass, String methodName, List<Object> args) {
-    public CacheKey(Class<?> entityClass, String methodName, Object... args){
+    public CacheKey(Class<?> entityClass, String methodName, Object... args) {
         this(entityClass, methodName, Arrays.asList(args));
     }
 }
--- src/main/java/com/example/project/repository/StudentRepository.java
+++ src/main/java/com/example/project/repository/StudentRepository.java
@@ -27,10 +27,10 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     @EntityGraph(attributePaths = {"violations", "room", "contract"})
     Student findStudentById(Long id);
 
-    @Query("SELECT s FROM Student s LEFT JOIN s.violations v " +
-            "WHERE (:age IS NULL OR s.age = :age) " +
-            "AND (:chs IS NULL OR s.chs = :chs) " +
-            "AND (:violationType IS NULL OR v.violationType = :violationType)")
+    @Query("SELECT s FROM Student s LEFT JOIN s.violations v "
+            + "WHERE (:age IS NULL OR s.age = :age) "
+            + "AND (:chs IS NULL OR s.chs = :chs) "
+            + "AND (:violationType IS NULL OR v.violationType = :violationType)")
     @EntityGraph(attributePaths = {"violations", "room", "contract"})
     Page<Student> findStudentsWithFiltersPaged(
             @Param("age") Integer age,
@@ -38,18 +38,18 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
             @Param("violationType") ViolationType violationType,
             Pageable pageable);
 
-    @Query(value = "SELECT DISTINCT s.* FROM students s " +
-            "LEFT JOIN student_violations sv ON s.id = sv.student_id " +
-            "LEFT JOIN violations v ON sv.violation_id = v.id " +
-            "WHERE (:age IS NULL OR s.age = :age) " +
-            "AND (:chs IS NULL OR s.chs = :chs) " +
-            "AND (:violationType IS NULL OR v.violation_type = :violationType)",
-            countQuery = "SELECT count(DISTINCT s.id) FROM students s " +
-                    "LEFT JOIN student_violations sv ON s.id = sv.student_id " +
-                    "LEFT JOIN violations v ON sv.violation_id = v.id " +
-                    "WHERE (:age IS NULL OR s.age = :age) " +
-                    "AND (:chs IS NULL OR s.chs = :chs) " +
-                    "AND (:violationType IS NULL OR v.violation_type = :violationType)",
+    @Query(value = "SELECT DISTINCT s.* FROM students s "
+            + "LEFT JOIN student_violations sv ON s.id = sv.student_id "
+            + "LEFT JOIN violations v ON sv.violation_id = v.id "
+            + "WHERE (:age IS NULL OR s.age = :age) "
+            + "AND (:chs IS NULL OR s.chs = :chs) "
+            + "AND (:violationType IS NULL OR v.violation_type = :violationType)",
+            countQuery = "SELECT count(DISTINCT s.id) FROM students s "
+                    + "LEFT JOIN student_violations sv ON s.id = sv.student_id "
+                    + "LEFT JOIN violations v ON sv.violation_id = v.id "
+                    + "WHERE (:age IS NULL OR s.age = :age) "
+                    + "AND (:chs IS NULL OR s.chs = :chs) "
+                    + "AND (:violationType IS NULL OR v.violation_type = :violationType)",
             nativeQuery = true)
     Page<Student> findStudentsByComplexCriteriaNativePaged(
             @Param("age") Integer age,
